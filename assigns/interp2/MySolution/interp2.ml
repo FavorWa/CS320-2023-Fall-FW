4e445006e97bd9cec6c19c9ce91cb5fda9b232fd
Favor Wariboko <66702423+FavorWa@users.noreply.github.com>
2023-12-05 20:20:32 -0500

    tester

--- assigns/interp2/MySolution/interp2.ml
+++ assigns/interp2/MySolution/interp2.ml
@@ -13,28 +13,18 @@ Notes:
 *)
 
 (*UTIL*)
-let is_digit c =
-  '0' <= c && c <= '9'
 let explode s =
   List.of_seq (String.to_seq s)
+
 let implode ls =
-  String.of_seq (List.to_seq ls)
- 
-  let boolToString (b : bool) : string =
-    if b then "True" else "False"
-  
-  let toString (c : const) : string =
-    match c with
-    | Int i -> string_of_int i
-    | Bool b -> boolToString b
-    | Unit -> "Unit"
+ String.of_seq (List.to_seq ls)
+
 let rec append_lists l1 l2 =
-  match l1 with
-  | [] -> l2
-  | h :: t -> h :: append_lists t l2
+ match l1 with
+ | [] -> l2
+ | h :: t -> h :: append_lists t l2
 
 
-(* abstract syntax tree of interp1 *)
 type const =
   | Int of int
   | Bool of bool
@@ -43,19 +33,16 @@ type const =
   | Closure of string * (string*const)list * com list
   
 and com =
-  | Push of const 
-  | Pop 
-  | Trace 
-  | Add | Sub | Mul | Div 
-  | And | Or | Not
-  | Lt | Gt 
-  | If of com list * com list
-  | Swap | Bind | Lookup 
-  | Fun of com list
+  | Push of const | Pop | Swap 
+  | Trace | Add | Sub | Mul
+  | Div | And | Or | Not
+  | Lt | Gt | If of com list * com list
+  | Bind | Lookup | Fun of com list
   | Call | Return
 
 and coms = com list
 
+ (* ------------------------------------------------------------ *)
  
  (* parsers for interp1 *)
 let parse_nat = 
@@ -73,7 +60,7 @@ let parse_unit =
   keyword "Unit" >> pure Unit
 
 let parse_sym =
-  let* sy = ((satisfy (char_isletter)) >>= fun c -> many (satisfy char_isalphanum) >|= fun cs -> c::cs) in pure (Sym (implode sy))
+  let* s = ((satisfy (char_isletter)) >>= fun c -> many (satisfy char_isalphanum) >|= fun cs -> c::cs) in pure (Sym (implode s))
  
 let parse_const =
   parse_int <|>
@@ -82,41 +69,35 @@ let parse_const =
   parse_sym
 
 
-  let parse_basic_command () =
-    (keyword "Push" >> parse_const >>= fun c -> pure (Push c)) <|>
-    (keyword "Pop" >> pure Pop) <|>
-    (keyword "Trace" >> pure Trace) <|>
-    (keyword "Add" >> pure Add) <|>
-    (keyword "Sub" >> pure Sub) <|>
-    (keyword "Mul" >> pure Mul) <|>
-    (keyword "Div" >> pure Div) <|>
-    (keyword "And" >> pure And) <|>
-    (keyword "Or" >> pure Or) <|>
-    (keyword "Not" >> pure Not) <|>
-    (keyword "Lt" >> pure Lt) <|>
-    (keyword "Gt" >> pure Gt) <|>
-    (keyword "Swap" >> pure Swap) <|>
-    (keyword "Trace" >> pure Trace) <|>
-    (keyword "Bind" >> pure Bind) <|>
-    (keyword "Lookup" >> pure Lookup) <|>
-    (keyword "Call" >> pure Call) <|>
-    (keyword "Return" >> pure Return)
-  
-  let rec parse_if_else () =
-    keyword "If" >> parse_coms () >>= fun c1 ->
-    keyword "Else" >> parse_coms () >>= fun c2 ->
-    keyword "End" >> pure (If (c1, c2))
-  
-  and parse_fun () =
-    keyword "Fun" >> parse_coms () >>= fun c1 ->
-    keyword "End" >> pure (Fun c1)
-  
-  and parse_com () =
-    parse_basic_command () <|>
-    parse_if_else () <|>
-    parse_fun ()
-  
-  and parse_coms () = many' (fun x -> parse_com x << keyword ";")
+let rec parse_com ()= 
+  (keyword "Push" >> parse_const >>= fun c -> pure (Push c)) <|>
+  (keyword "Pop" >> pure Pop) <|>
+  (keyword "Trace" >> pure Trace) <|>
+  (keyword "Add" >> pure Add) <|>
+  (keyword "Sub" >> pure Sub) <|>
+  (keyword "Mul" >> pure Mul) <|>
+  (keyword "Div" >> pure Div) <|>
+  (keyword "And" >> pure And) <|>
+  (keyword "Or" >> pure Or) <|>
+  (keyword "Not" >> pure Not) <|>
+  (keyword "Lt" >> pure Lt) <|>
+  (keyword "Gt" >> pure Gt) <|>
+  (keyword "Swap" >> pure Swap) <|>
+  (keyword "Trace" >> pure Trace) <|>
+  (keyword "Bind" >> pure Bind) <|>
+  (keyword "Lookup" >> pure Lookup) <|>
+  (keyword "Call" >> pure Call) <|>
+  (keyword "Return" >> pure Return) <|>
+  parse_ifelse() <|>
+  parse_fun()
+and parse_coms ()= many' (fun x -> parse_com x << keyword ";")
+and parse_ifelse ()=
+  keyword "If" >> parse_coms () >>= fun c1 ->
+  keyword "Else" >> parse_coms () >>= fun c2 -> 
+  keyword "End" >>
+  pure (If (c1, c2))
+and parse_fun ()=
+  keyword "Fun" >> parse_coms () >>= fun c1 -> keyword "End" >> pure (Fun c1)
 
 
 
@@ -218,50 +199,64 @@ let rec eval (s : stack) (t : trace) (e: env) (p : prog) : trace =
     | _ :: _ :: s0         (* GtError1 *) -> eval [] ("Panic" :: t) e []
     | []                   (* GtError2 *) -> eval [] ("Panic" :: t) e []
     | _ :: []              (* GtError3 *) -> eval [] ("Panic" :: t) e [])
-    | Swap :: p0 ->
-      (match s with
-       | x :: y :: lst -> eval (y :: x :: lst) t e p0
-       | _             -> eval [] ("Panic" :: t) e [])
-    | Bind :: p0 ->
-      (match s with
-       | Sym x :: v :: s0 -> eval s0 t ((x, v) :: e) p0
-       | _                -> eval [] ("Panic" :: t) e [])
-    | Lookup :: p0 ->
-      (match s with
-       | Sym x :: s0 ->
-         let rec find env key =
-           match env with
-           | []             -> None
-           | (k, v) :: rest -> if k = key then Some v else find rest key
-         in
-         (match find e x with
-          | Some v -> eval (v :: s0) t e p0
-          | None   -> eval [] ("Panic" :: t) e [])
-       | _ -> eval [] ("Panic" :: t) e [])
-    | If (c1, c2) :: p0 ->
-      (match s with
-       | Bool b :: s0 ->
-         eval s0 t e (append_lists (if b then c1 else c2) p0)
-       | _ -> eval [] ("Panic" :: t) e [])
-    | Fun c1 :: p0 ->
-      (match s with
-       | Sym x :: s0 -> eval (Closure (x, e, c1) :: s0) t e p0
-       | _ -> eval [] ("Panic" :: t) e [])
-    | Call :: p0 ->
-      (match s with
-       | Closure (f, v, c) :: a :: s0 ->
-         eval (a :: Closure (string_append "cc_foo(" (toString a) ^ ")", e, p0) :: s0)
-              t ((f, Closure (f, v, c)) :: v) c
-       | _ :: a :: s0 -> eval [] ("Panic" :: t) e []
-       | h :: []      -> eval [] ("Panic" :: t) e []
-       | []           -> eval [] ("Panic" :: t) e [])
-    | Return :: p0 ->
-      (match s with
-       | Closure (f, v, c) :: a :: s0 -> eval (a :: s0) t v c
-       | _ :: a :: s0 -> eval [] ("Panic" :: t) e []
-       | h :: []      -> eval [] ("Panic" :: t) e []
-       | []           -> eval [] ("Panic" :: t) e [])
+ | Swap :: p0 ->
+   (match s with
+    | x :: y :: lst -> eval  (y :: x :: lst) t e p0
+    | [] (* SwapError1 *) -> eval [] ("Panic" :: t) e []
+    | x :: [] (* SwapError2 *) -> eval [] ("Panic" :: t) e [])
+ | Bind :: p0 -> 
+   (match s with
+    | Sym x :: v :: s0 -> eval s0 t ((x, v) :: e) p0
+    | _ -> eval [] ("Panic" :: t) e [])
+ | Lookup :: p0 -> 
+   (match s with
+    | Sym x :: s0 -> 
+     let rec find env (key) =
+        match env with
+        | [] -> None
+        | (k, v) :: rest -> if k = key then Some v else find rest key
+     in
+     (match find e x with
+        | Some v -> eval (v :: s0) t e p0
+        | None -> eval [] ("Panic" :: t) e [])
+    | _ -> eval [] ("Panic" :: t) e [])
+ | If (c1, c2) :: p0 -> 
+   (match s with
+    | Bool b :: s0 ->  if b then eval s0 t e (append_lists c1 p0) else eval s0 t e (append_lists c2 p0)
+    | _ -> eval [] ("Panic" :: t) e [])
+ | Fun c1 :: p0 ->
+   (match s with
+    | Sym x :: s0 -> let s1 = Closure(x, e, c1) :: s0 in eval s1 t e p0
+    | _ -> eval [] ("Panic" :: t) e [])
+ | Call :: p0 ->
+   (match s with
+    | Closure (f, v, c) :: a :: s0 -> eval (a :: Closure(string_append (string_append "cc_foo(" (toString(a))) ")",e,p0) :: s0) t ((f,Closure(f,v,c)) :: v) c
+    | _ :: a :: s0 -> eval [] ("Panic" :: t) e []
+    | h :: [] -> eval [] ("Panic" :: t) e []
+    | [] -> eval [] ("Panic" :: t) e [])
+ | Return :: p0 ->
+   (match s with
+    | Closure (f, v, c) :: a :: s0 -> eval (a :: s0) t v c
+    | _ :: a :: s0 -> eval [] ("Panic" :: t) e []
+    | h :: [] -> eval [] ("Panic" :: t) e []
+    | [] -> eval [] ("Panic" :: t) e []) 
   
+   
+(*\
+V, P
+Closure(f, v, c):: a :: stack
+
+Stack:
+a:: Closure ("cc", V, P) :: stack
+
+Env:
+(f, Closure(f, v, c)) :: v
+
+
+\*)
+ 
+
+(* Tuple with (Name, Environment, Code)*)
 (* ------------------------------------------------------------ *)
 
 (* putting it all together [input -> parser -> eval -> output] *)
